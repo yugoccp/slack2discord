b885868349393ca74c0bb41aa044be151cb1162f
Yugo Sakamoto <yugoccp@gmail.com>
2020-06-22 00:38:51 -0300

    f Add emoji unicode parser

--- src/slackApi.js
+++ src/slackApi.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const punycode = require('punycode');
+const emoji_data = require('../rsc/emoji.json');
 
 const getFile = async url => {
     return axios({
@@ -8,6 +10,28 @@ const getFile = async url => {
     });
 }
 
+const emojiToUnicode = text => {
+
+  var emoji_re = /\:([a-zA-Z0-9\-_\+]+)\:(?:\:([a-zA-Z0-9\-_\+]+)\:)?/g;
+
+  var new_text = text;
+
+  // Find all Slack emoji in the message
+  while(match=emoji_re.exec(text)) {
+    var ed = emoji_data.find(function(el){
+      return el.short_name == match[1];
+    });
+    if(ed) {
+      var points = ed.unified.split("-");
+      points = points.map(function(p){ return parseInt(p, 16) });
+      new_text = new_text.replace(match[0], punycode.ucs2.encode(points));
+    }
+  }
+
+  return new_text;
+}
+
 module.exports = {
-  getFile
+  getFile,
+  emojiToUnicode
 }
