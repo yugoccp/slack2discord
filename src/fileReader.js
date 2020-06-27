4fbd9e37700e07384d793bcc42fd2d768e493eb6
Yugo Sakamoto <yugoccp@gmail.com>
2020-06-27 18:17:27 -0300

    r Add jsDoc

--- src/fileReader.js
+++ src/fileReader.js
@@ -9,6 +9,10 @@ const {
 const OUTPUT_PATH = '../out';
 const DONE_PATH = '../done';
 
+/**
+ * Retrive Slack users from users.json mapped by Id.
+ * @param {string} backupPath 
+ */
 const getUsersById = async (backupPath) => {
   const file = await fs.promises.readFile(`${backupPath}/users.json`);
   return JSON.parse(file).reduce((acc, value) => {
@@ -17,6 +21,10 @@ const getUsersById = async (backupPath) => {
   }, {});
 }
 
+/**
+ * Retrieve Slack channels from channels,json by Id.
+ * @param {string} backupPath 
+ */
 const getChannelsById = async (backupPath) => {
   const file = await fs.promises.readFile(`${backupPath}/channels.json`);
   return JSON.parse(file).reduce((acc, ch) => {
@@ -25,6 +33,10 @@ const getChannelsById = async (backupPath) => {
   }, {});
 }
 
+/**
+ * Retrieve filtered directories name.
+ * @param {string} path 
+ */
 const getDirNames = async(path) => {
   const outputDir = await fs.promises.readdir(path, { withFileTypes: true });
   return outputDir
@@ -35,35 +47,64 @@ const getDirNames = async(path) => {
     .map(dir => dir.name);
 }
 
+/**
+ * Retrieve Slack messages channel directories name.
+ * @param {string} backupPath 
+ */
 const getSlackDirs = async (backupPath) => {
   return await getDirNames(`${backupPath}`, { withFileTypes: true });
 }
 
+/**
+ * Retrieve Output messages directories name.
+ */
 const getOutputDirs = async () => {
   return await getDirNames(`${__dirname}/${OUTPUT_PATH}`, { withFileTypes: true });
 }
 
-const getSlackFiles = async (backupPath, channel) => {
-  const channelPath = path.resolve(backupPath, channel);
+/**
+ * Retrieve Slack files from `channelName` directory.
+ * @param {string} backupPath 
+ * @param {string} channelName 
+ */
+const getSlackFiles = async (backupPath, channelName) => {
+  const channelPath = path.resolve(backupPath, channelName);
   const files = await fs.promises.readdir(channelPath);
   return files.map(f => path.resolve(channelPath, f));
 }
 
-const getOutputFiles = async (dir) => {
-  const channelPath = path.resolve(`${__dirname}/${OUTPUT_PATH}`, dir);
+/**
+ * Retrieve Output files from `folderName` directory. 
+ * @param {string} folderName
+ */
+const getOutputFiles = async (folderName) => {
+  const channelPath = path.resolve(`${__dirname}/${OUTPUT_PATH}`, folderName);
   const files = await fs.promises.readdir(channelPath);
   return files.map(f => path.resolve(channelPath, f));
 }
 
+/**
+ * Retrive messages from `path`.
+ * @param {string} path 
+ */
 const getMessages = async path => {
   const data = await fs.promises.readFile(path);
   return JSON.parse(data);
 }
 
+/**
+ * Create folder under 'done/' directory.
+ * @param {string} folderName 
+ */
 const createDoneFolder = async folderName => {
   await fs.promises.mkdir(`${__dirname}/${DONE_PATH}/${folderName}`, { recursive: true });
 }
 
+/**
+ * Move file to 'done/' folder.
+ * @param {string} folderName 
+ * @param {string} filePath 
+ */
 const moveToDone = async (folderName, filePath) => {
   const filename = path.basename(filePath)
   const donePath = `${__dirname}/${DONE_PATH}/${folderName}/${filename}`;
