62e382bb5fe9782297faa044b22bdd4d038d94fd
Yugo Sakamoto <yugoccp@gmail.com>
2020-09-09 23:53:13 -0300

    R!!! Join 'run' and 'send' command

--- src/command.js
+++ src/command.js
@@ -16,8 +16,12 @@ program
   .option('-c, --config [value]', 'Configuration file path')
   .option('-t, --token [value]', 'Discord bot token')
   .option('--server [value]', 'Discord server (guild) ID')
-  .action(async ({config, ...options}) => {
+  .option('--only-parse', 'Only parses the messages to check')
+  .action(async ({config, onlyParse, ...options}) => {
     
+    options.include = options.include ? options.include.split(',') : [];
+    options.exclude = options.exclude ? options.exclude.split(',') : [];
+
     const configFile = config && await fs.promises.readFile(config);
     const configOptions = configFile ? JSON.parse(configFile) : {};
     const { source, token, server, out, include, exclude, parentChannel } = {
@@ -25,19 +29,18 @@ program
       ...options
     }
 
-    assert.ok(token, 'Please, provide your Discord Bot token to continue...');
-    assert.ok(server, 'Please, provide your Server (Guild) ID to continue...');
-
     const sourcePath = source || process.cwd();
-    const outPath = out || './.s2d_tmp/out';
+    const outPath = out || `${sourcePath}/.s2d/out`;
     const pChannel = parentChannel || 'MIGRATION';
-    const includeList = include ? include.split(',') : [];
-    const excludeList = exclude ? exclude.split(',') : [];
-
     
     console.log(`Parsing messages from ${sourcePath}`);
 
-    await parse(sourcePath, outPath, includeList, excludeList);
+    await parse(sourcePath, outPath, include, exclude);
+
+    if (onlyParse) return;
+
+    assert.ok(token, 'Please, provide your Discord Bot token to continue...');
+    assert.ok(server, 'Please, provide your Server (Guild) ID to continue...');
 
     console.log(`Sending messages from ${outPath} to Discord`);
 
@@ -45,22 +48,4 @@ program
 
   });
 
-program
-  .command('parse')
-  .option('-s, --source [value]', 'Slack messages source path')
-  .option('-o, --out [value]', 'Parsed message output path')
-  .option('-i, --include [value]', 'Include channels')
-  .option('-e, --exclude [value]', 'Exclude channels')
-  .action(async ({ source, out, include, exclude }) => {
-    
-    const sourcePath = source || process.cwd();
-    const outPath = out || './out';
-    const includeList = include ? include.split(',') : [];
-    const excludeList = exclude ? exclude.split(',') : [];
-
-    console.log(`Parsing messages from ${sourcePath}`);
-
-    await parse(sourcePath, outPath, includeList, excludeList);
-  });
-  
 program.parse(process.argv);
